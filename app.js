082cf13c907edb662022b85dcfbd34756923f345
Jadam <mrjadaml@gmail.com>
2016-04-03 21:37:50 -0700

    added cookie-session, es6-ified, index view updates

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
@@ -18,20 +21,23 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+  name: 'session',
+  keys: [process.env['SECRET_KEY']]
+}));
 app.use(passport.initialize());
+app.use(passport.session());
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser((user, done) => {
   done(null, user);
 });
 
-passport.deserializeUser(function(id, done) {
+passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
@@ -40,20 +46,21 @@ passport.use(new LinkedInStrategy({
   clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
   callbackURL: process.env.HOST + '/auth/linkedin/callback',
   scope: ['r_emailaddress', 'r_basicprofile'],
-}, function(accessToken, refreshToken, profile, done) {
-  process.nextTick(function() {
-    return done(null, profile);
-  });
+  state: true,
+}, (accessToken, refreshToken, profile, done) => {
+    done(null, {
+      id: profile.id,
+      displayName: profile.displayName,
+      token: accessToken
+    });
 }));
 
 app.use('/', routes);
 app.use('/users', users);
 
-app.get('/auth/linkedin',
-  passport.authenticate('linkedin', { state: 'SOME STATE' }),
-  function (req, res) {
+app.get('/auth/linkedin', passport.authenticate('linkedin'), (req, res) => {
 
-  });
+});
 
 app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
   successRedirect: '/',
@@ -61,7 +68,7 @@ app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
 }));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -72,7 +79,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -83,7 +90,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
