28fa54019157f057b8df2881770a3f459d13ab34
Jadam <mrjadaml@gmail.com>
2016-04-05 10:47:39 -0700

    update routes and views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const express = require('express');
+const router = express.Router();
+const unirest = require('unirest');
+
+router.get('/', (req, res, next) => {
+  if (req.isAuthenticated()) {
+    unirest.get('https://api.linkedin.com/v1/people/~:(id,num-connections,picture-url)')
+      .header('Authorization', 'Bearer' + req.user.token)
+      .header('x-li-format', 'json')
+      .end(response => {
+        console.log(response)
+        res.render('index', { profile: response.body });
+      })
+  } else {
+    res.render('index', { });
+  }
 });
 
 module.exports = router;
